47ed5a74ea3112f8698a5c0a4d804b0e4c042722
conner <gorman.connerm@gmail.com>
2024-07-03 23:08:37 -0400

    use env to respond

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/demo-app')
 def hello():
-    return "Hello, Kubernetes!"
+    current_env = os.env.get("FLASK_ENV")
+    return f"Hello, {current_env}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
