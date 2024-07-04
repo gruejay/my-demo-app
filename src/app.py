898428fcfbb04f864d0e3c9bee9db5affc76153e
conner <gorman.connerm@gmail.com>
2024-07-04 13:09:29 -0400

    change title of site

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     current_env = os.getenv("FLASK_ENV")
-    return f"Hello, {current_env}!"
+    return f"Hello there, {current_env}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
