815b95a016397df56a4c8cf3da7fce7291e2883a
conner <gorman.connerm@gmail.com>
2024-07-09 20:35:00 -0400

    change monorepo name

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     current_env = os.getenv("FLASK_ENV")
-    return f"Hello there, {current_env}!"
+    return f"Hello there, {current_env}! THis is deployed from a monorepo"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
